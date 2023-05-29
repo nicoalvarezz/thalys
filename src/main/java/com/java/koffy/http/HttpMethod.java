637c729b013130e8310c461154531fe084a9d8fa
Nico <nico@Nicolass-MacBook-Air.local>
2023-05-29 18:33:14 +0200

    minor changes

--- src/main/java/com/java/koffy/http/HttpMethod.java
+++ src/main/java/com/java/koffy/http/HttpMethod.java
@@ -4,9 +4,24 @@ package com.java.koffy.http;
  * Http Method enum class.
  */
 public enum HttpMethod {
-    GET,
-    POST,
-    PUT,
-    PATCH,
-    DELETE
+
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    PATCH("PATCH"),
+    DELETE("DELETE");
+
+    private final String method;
+
+    HttpMethod(String method) {
+        this.method = method;
+    }
+
+    /**
+     * Returns the String value of a specific method
+     * @return {@link String} method value
+     */
+    public String get() {
+        return method;
+    }
 }
