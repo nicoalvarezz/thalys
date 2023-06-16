d98513f1fe366fb855db17f49e5ea7a974be562e
Nico <nico@Nicolass-MacBook-Air.local>
2023-06-16 21:24:11 +0200

    minor change

--- src/main/java/com/java/koffy/Main.java
+++ src/main/java/com/java/koffy/Main.java
@@ -12,17 +12,17 @@ public class Main {
                 .build());
 
         app.router().post("/test-post", (request) ->
-                ResponseEntity.jsonResponse(request.getPostData()).status(HttpStatus.OK).build(), QuickTest.class);
+                ResponseEntity.jsonResponse(request.getPostData()).status(HttpStatus.OK).build());
 
         app.router().get("/test", (request) ->
             ResponseEntity.jsonResponse(request.getQueryData()).status(HttpStatus.OK).build());
 
         app.router().get("/redirect", (request) -> ResponseEntity.redirectResponse("/test/5"));
 
-        app.router().post("/test-email", (request) -> {
-            QuickTest quickTest = request.getSerialized(QuickTest.class);
-            return ResponseEntity.textResponse(quickTest.getEmail()).build();
-        }, QuickTest.class);
+//        app.router().post("/test-email", (request) -> {
+//            QuickTest quickTest = request.getSerialized(QuickTest.class);
+//            return ResponseEntity.textResponse(quickTest.getEmail()).build();
+//        }, QuickTest.class);
 
 //        app.router().get("/middlewares", (request -> ResponseEntity.jsonResponse(new HashMap<>() {{
 //            put("message", "OK");
